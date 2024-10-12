67eeba0909ad32828d93800b407ffc060f83d5a6
Venu <venugopalchavate@gmail.com>
2024-10-12 21:33:58 +0900

    adding workloadidentity management in cluster

--- terraform/modules/cluster.tf
+++ terraform/modules/cluster.tf
@@ -1,8 +1,13 @@
+data "google_project" "project" {
+}
 resource "google_container_cluster" "create_cluster" {
   name               = var.cluster_name
   location           = var.zone
   initial_node_count = 3
   node_locations = [var.zone2]
+  workload_identity_config {
+      workload_pool = "${data.google_project.project.project_id}.svc.id.goog"
+    }
   addons_config {
     http_load_balancing{
       disabled = false
