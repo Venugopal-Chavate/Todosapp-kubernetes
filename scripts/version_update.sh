6dd8d2954bbb5d4693d76fd524a84a7f9d3ccbac
Venu <venugopalchavate@gmail.com>
2024-10-13 19:52:28 +0900

    testing versioning

--- scripts/version_update.sh
+++ scripts/version_update.sh
@@ -24,18 +24,18 @@ echo "Current Version: $CURRENT_VERSION"
 CURRENT_VERSION_PARTS=(${CURRENT_VERSION//./ })
 
 # get number parts
-VNUM1=${CURRENT_VERSION_PARTS[0]}
+VNUM1=${CURRENT_VERSION_PARTS[0]//v/}
 VNUM2=${CURRENT_VERSION_PARTS[1]}
 VNUM3=${CURRENT_VERSION_PARTS[2]}
 
-if [[ $VERSION == 'major' ]]
-then
-  VNUM1=v$((VNUM1+1))
-elif [[ $VERSION == 'minor' ]]
-then
+if [[ $VERSION == 'major' ]]; then
+  VNUM1=$((VNUM1+1))
+  VNUM2=0
+  VNUM3=0
+elif [[ $VERSION == 'minor' ]]; then
   VNUM2=$((VNUM2+1))
-elif [[ $VERSION == 'patch' ]]
-then
+  VNUM3=0
+elif [[ $VERSION == 'patch' ]]; then
   VNUM3=$((VNUM3+1))
 else
   echo "No version type (https://semver.org/) or incorrect type specified, try: -v [major, minor, patch]"
@@ -43,7 +43,7 @@ else
 fi
 
 # create new tag
-NEW_TAG="$VNUM1.$VNUM2.$VNUM3"
+NEW_TAG="v$VNUM1.$VNUM2.$VNUM3"
 echo "($VERSION) updating $CURRENT_VERSION to $NEW_TAG"
 
 # get current hash and see if it already has a tag
