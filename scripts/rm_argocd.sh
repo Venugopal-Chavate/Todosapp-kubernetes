e5f82254d434697a6f4b1d026a9f6436ea818e9f
Venu <venugopalchavate@gmail.com>
2024-10-13 22:55:09 +0900

    adding tag flows

--- scripts/rm_argocd.sh
+++ scripts/rm_argocd.sh
@@ -1,4 +1,3 @@
-kubectl delete -f ./argocd_manifests/argocd_service_config.yaml
 kubectl -n argocd delete -f ./argocd_manifests/argocd_install.yaml
 kubectl delete -f ./argocd_manifests/argo_namespace.yaml
 
