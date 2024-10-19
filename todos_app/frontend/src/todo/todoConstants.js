97152b7ad11deb298a6664530bd6bc40bd959f44
Venu <venugopalchavate@gmail.com>
2024-10-19 22:40:59 +0900

    testing

--- todos_app/frontend/src/todo/todoConstants.js
+++ todos_app/frontend/src/todo/todoConstants.js
@@ -1,8 +1,6 @@
-// const apiUrl = `http://${process.env.REACT_APP_BACKEND}:${process.env.REACT_APP_API_PORT}/api/todos`;
-const apiUrl = `//backend-service/api/todos`;
 export const TODO_CONSTANTS = {
 	DESCRIPTION_CHANGED:"DESCRIPTION_CHANGED",
 	TODO_SEARCHED: "TODO_SEARCHED",
 	TODO_CLEAR: "TODO_CLEAR",
-	URL: apiUrl
+	URL: `//backend-service/api/todos`
 }
