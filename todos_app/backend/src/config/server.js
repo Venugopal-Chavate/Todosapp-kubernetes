b574122ebe610be9dbdc86a6530e6c7808bc9957
Venu <venugopalchavate@gmail.com>
2024-10-26 23:18:12 +0900

    rebuild docker

--- todos_app/backend/src/config/server.js
+++ todos_app/backend/src/config/server.js
@@ -9,7 +9,6 @@ const allowCors = require('./cors');
 server.use(bodyParser.urlencoded({ extended: true }));
 server.use(bodyParser.json());
 server.use(allowCors);
-
 server.listen(port, () => {
   console.log(`BACKEND is running on port ${port}`);
 });
